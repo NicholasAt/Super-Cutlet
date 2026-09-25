f38822f0bceaaf8e55aac7a8077237a5a15bbf78
NicholasAt <1965175291asd@gmail.com>
2026-09-25 15:56:15 +0300

    fix immortal saw

--- Assets/CodeBase/Constants.cs
+++ Assets/CodeBase/Constants.cs
@@ -5,5 +5,6 @@ namespace CodeBase
     public class Constants : MonoBehaviour
     {
         public const float WindowAnimationSpeed = 5;
+        public const string PlayerTag = "Player";
     }
 }
---
+++ Assets/CodeBase/Logic/DestroyOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Logic
+{
+    public class DestroyOnCollision : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.CompareTag(Constants.PlayerTag) == false)
+                Destroy(gameObject);
+        }
+    }
+}
